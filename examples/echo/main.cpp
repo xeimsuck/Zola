e4bab99124f1e0474c92d7575ddac1a3b67c5b4b
xeim <xeimdonot@gmail.com>
2024-08-12 16:45:00 +0000

    changed echo code

--- examples/echo/main.cpp
+++ examples/echo/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <Zola/Zola.hpp>
 
+using namespace Zola;
+
 int main(int argc, char*argv[]){
-    decltype(auto) bot = Zola::Bot::init("7047048031:AAEbb5yTcq5Gd86ecYjnsUg2Qore3pgAAzg");
+    decltype(auto) bot = Zola::Bot::init("YOUR TOKEN");
+    bot.getEventHandler().getMessageHandler().addAny([](const Objects::Message& msg){
+        std::cout << "Message: "<< msg.text.value_or("") << std::endl;
+    });
+    bot.getEventHandler().getCommandHandler().addAny([](const Objects::Message& msg){
+        std::cout << "Command: "<< msg.text.value_or("") << std::endl;
+    });
     try {
         bot.run();
     } catch (const std::exception& ex){
