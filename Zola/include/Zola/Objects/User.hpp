755ab207454a3605a1ef7c5f9294fef95ba33596
xeim <xeimdonot@gmail.com>
2024-08-13 17:58:42 +0000

    added all arguments in user

--- Zola/include/Zola/Objects/User.hpp
+++ Zola/include/Zola/Objects/User.hpp
@@ -1,11 +1,11 @@
 /*!
-    \file
-    \brief User object
-    \author Xeim
-    \version 0.1
-    \data 09.08.24
+    @file
+    @brief User object
+    @author Xeim
+    @version 0.1
+    @data 09.08.24
 
-    This struct contains information about user
+    This object represents a Telegram user or bot.
  */
 
 #ifndef ZOLA_OBJECTS_USER_HPP
@@ -17,12 +17,87 @@
 
 namespace Zola::Objects{
     struct User {
+        /*!
+         * @brief Default constructor
+        */
+        User() = default;
+
+        /*!
+         * @brief Parse json
+         * @param data Json user
+         */
         explicit User(const nlohmann::json& data);
-        long id;
-        bool is_bot;
+
+        /*!
+         * @brief Unique identifier for this user or bot.
+         *
+         * This number may have more than 32 significant bits and some
+         * programming languages may have difficulty/silent defects
+         * in interpreting it. But it has at most 52 significant bits,
+         * so a 64-bit integer or double-precision float type are safe for
+         * storing this identifier.
+        */
+        long id=0;
+
+        /*!
+         * @brief True, if this user is a bot
+         */
+        bool is_bot=false;
+
+        /*!
+         * @brief User's or bot's first name
+         */
         std::string first_name;
+
+        /*!
+         * @brief User's or bot's last name
+         */
         std::optional<std::string> last_name;
+
+        /*!
+         * @brief User's or bot's username
+         */
         std::optional<std::string> username;
+
+        /*!
+         * @brief IETF language tag of the user's language
+         */
+        std::optional<std::string> language_code;
+
+        /*!
+         *  @brief True, if this user is a Telegram Premium user
+         */
+        std::optional<bool> is_premium;
+
+        /*!
+         * @brief True, if this user added the bot to the attachment menu
+         */
+        std::optional<bool> added_to_attachment_menu;
+
+        /*!
+         * @brief True, if the bot can be invited to groups.
+         */
+        std::optional<bool> can_join_groups;
+
+        /*!
+         * @brief True, if privacy mode is disabled for the bot.
+         */
+        std::optional<bool> can_read_all_group_messages;
+
+        /*!
+         * @brief True, if the bot supports inline queries.
+         */
+        std::optional<bool> supports_inline_queries;
+
+        /*!
+         * @brief True, if the bot can be connected to a Telegram Business account to receive its messages.
+         */
+        std::optional<bool> can_connect_to_business;
+
+        /*!
+         * @brief True, if the bot has a main Web App.
+         */
+        std::optional<bool> has_main_web_app;
     };
 }
 
--- Zola/src/Objects/User.cpp
+++ Zola/src/Objects/User.cpp
@@ -11,4 +11,28 @@ Zola::Objects::User::User(const nlohmann::json& data){
     if(data.contains("last_name")){
         last_name = data["last_name"];
     }
+    if(data.contains("language_code")){
+        language_code = data["language_code"];
+    }
+    if(data.contains("is_premium")){
+        is_premium = data["is_premium"];
+    }
+    if(data.contains("added_to_attachment_menu")){
+        added_to_attachment_menu = data["added_to_attachment_menu"];
+    }
+    if(data.contains("can_join_groups")){
+        can_join_groups = data["can_join_groups"];
+    }
+    if(data.contains("can_read_all_group_messages")){
+        can_read_all_group_messages = data["can_read_all_group_messages"];
+    }
+    if(data.contains("supports_inline_queries")){
+        supports_inline_queries = data["supports_inline_queries"];
+    }
+    if(data.contains("can_connect_to_business")){
+        can_connect_to_business = data["can_connect_to_business"];
+    }
+    if(data.contains("has_main_web_app")){
+        has_main_web_app = data["has_main_web_app"];
+    }
 }
